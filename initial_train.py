Fix loss computation for new target
- Computing confusion matrix now use function from pytorch_lightning
packages

initial_train.py
@@ -1,5 +1,5 @@
-import numpy as np
 import torch
+import numpy as np
 from tqdm import tqdm
 from torch.utils.data import DataLoader, Subset
 
@@ -8,15 +8,25 @@ from nims_dataset import NIMSDataset, ToTensor
 from nims_trainer import NIMSTrainer
 #from nims_variable import parse_variables
 
-try:
-    from torchsummary import summary
-except:
-    pass
+import os
+
+def create_results_dir(experiment_name):
+    # Base results directory
+    results_dir = os.path.join('./results', experiment_name)
+    if not os.path.isdir(results_dir):
+        os.mkdir(results_dir)
+
+    # Create evaluation directory if not
+    eval_dir = os.path.join(results_dir, 'eval')
+    if not os.path.isdir(eval_dir):
+        os.mkdir(eval_dir)
+
+    # Create comparison_graph directory if not
+    graph_dir = os.path.join(results_dir, 'comparison_graph')
+    if not os.path.isdir(graph_dir):
+        os.mkdir(graph_dir)
 
 if __name__ == '__main__':
-    # Set the number of threads in pytorch
-    torch.set_num_threads(3)
-
     # Select start and end date for train and valid
     train_date = select_date()
     valid_date = {'year': 2020, 'start_month': 6, 'start_day': 1,
@@ -45,6 +55,7 @@ if __name__ == '__main__':
                                      train=True,
                                      transform=ToTensor())
 
+    # Valid dataset
     nims_valid_dataset = NIMSDataset(model=args.model,
                                      model_utc=args.model_utc,
                                      window_size=args.window_size,
@@ -89,7 +100,7 @@ if __name__ == '__main__':
     train_loader = DataLoader(nims_train_dataset, batch_size=args.batch_size,
                               shuffle=True, num_workers=args.num_workers,
                               pin_memory=True)
-    valid_loader = DataLoader(nims_valid_dataset, batch_size=args.batch_size,
+    valid_loader = DataLoader(nims_valid_dataset, batch_size=1,
                               shuffle=False, num_workers=args.num_workers,
                               pin_memory=True)
 

nims_dataset.py
@@ -1,12 +1,10 @@
 import torch
 from torch.utils.data import Dataset
-
-from nims_util import *
-import torchvision.transforms as transforms
-
-from datetime import datetime, timedelta
 import numpy as np
 
+from nims_util import *
+
+from datetime import datetime, timedelta
 import os
 
 __all__ = ['NIMSDataset', 'ToTensor']
@@ -34,10 +32,6 @@ class NIMSDataset(Dataset):
         
         self._data_path_list, self._gt_path = self.__set_path()
 
-    @property
-    def gt_path(self):
-        return self._gt_path
-        
     def __set_path(self):
         root, dirs, _ = next(os.walk(os.path.join(self.root_dir, str(self.date['year'])), topdown=True))
         
@@ -257,6 +251,7 @@ class NIMSDataset(Dataset):
 
         return gt
 
+    # XXX: Need to fix for new training target data
     def get_real_gt(self, idx):
         gt_path = self._gt_path[idx]
         real_gt = np.load(gt_path)

nims_logger.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
-import sys
 import os
 
 from nims_util import NIMSStat
@@ -21,8 +20,8 @@ class NIMSLogger:
         print_stat: Print one epoch stat
         latest_stat: Return straing of one "instance(batch)" stat
         """
-        self.stn_codi = stn_codi
-        self.num_stn = len(stn_codi)
+        self.num_pixels = 512 * 512     # # of pixels in training target data
+        self.num_stn = len(stn_codi)    # # of stations in testing target data
         self.test_date_dict = dict()
 
         if test_date_list != None:
@@ -35,6 +34,7 @@ class NIMSLogger:
             self.test_year = int(test_date_path.split('/')[-1][0:4])
         
         self.num_update = 0
+        self.total_points = 0
 
         # Initialize one epoch stat dictionary
         self.one_epoch_stat = OneTargetStat(loss, correct, macro_f1, micro_f1, hit, miss, fa, cn)
@@ -49,7 +49,7 @@ class NIMSLogger:
     def update(self, loss=None, correct=None,
                macro_f1=None, micro_f1=None,
                hit=None, miss=None, fa=None, cn=None,
-               target_time=None, test=False):
+               target_time=None, mode='train'):
 
         if loss != None:
             try:
@@ -102,8 +102,12 @@ class NIMSLogger:
 
         num_batch = target_time.shape[0]
         self.num_update += num_batch
+        if mode == 'train':
+            self.total_points += (num_batch * self.num_pixels)
+        elif (mode == 'valid') or (mode == 'test'):
+            self.total_points += (num_batch * self.num_stn)
 
-        if test:
+        if mode == 'test':
             for b in range(num_batch):
                 utc_year = int(target_time[b][0])
                 utc_month = int(target_time[b][1])
@@ -150,9 +154,7 @@ class NIMSLogger:
                     for col in self.daily_df.columns:
                         self.daily_df[col].values[:] = 0
     
-    def print_stat(self, test=False):
-        total_stn = self.num_update * self.num_stn
-
+    def print_stat(self, mode):
         stat_str = ''
         
         try:
@@ -162,7 +164,7 @@ class NIMSLogger:
             pass
 
         try:
-            acc = (self.one_epoch_stat.correct / total_stn) * 100
+            acc = (self.one_epoch_stat.correct / self.total_points) * 100
             stat_str += "[{:12s}] {:.3f}%\n".format('accuracy', acc)
         except:
             pass
@@ -178,11 +180,15 @@ class NIMSLogger:
             pass
 
         try:
-            csi = self.one_epoch_stat.hit / (self.one_epoch_stat.hit + self.one_epoch_stat.miss + self.one_epoch_stat.fa)
-            pod = self.one_epoch_stat.hit / (self.one_epoch_stat.hit + self.one_epoch_stat.miss)
-            far = self.one_epoch_stat.fa / (self.one_epoch_stat.hit + self.one_epoch_stat.fa)
-            bias = (self.one_epoch_stat.hit + self.one_epoch_stat.fa) / (self.one_epoch_stat.hit + self.one_epoch_stat.miss)
-            f1 = (2 * self.one_epoch_stat.hit) / ((2 * self.one_epoch_stat.hit) + self.one_epoch_stat.fa + self.one_epoch_stat.miss)
+            hit = self.one_epoch_stat.hit
+            miss = self.one_epoch_stat.miss
+            fa = self.one_epoch_stat.fa
+
+            csi = hit / (hit + miss + fa) if (hit + miss + fa) > 0 else 0.0
+            pod = hit / (hit + miss) if (hit + miss) > 0 else 0.0
+            far = fa / (hit + fa) if (hit + fa) > 0 else 0.0
+            bias = (hit + fa) / (hit + miss) if (hit + miss) > 0 else 0.0
+            f1 = (2 * hit) / ((2 * hit) + fa + miss) if ((2 * hit) + fa + miss) > 0 else 0.0
 
             stat_str += "[{:12s}] {:.5f}\n".format('pod', pod)
             stat_str += "[{:12s}] {:.5f}\n".format('csi', csi)
@@ -191,24 +197,29 @@ class NIMSLogger:
             stat_str += "[{:12s}] {:.5f}\n".format('bias', bias)
 
             epoch_stat = NIMSStat(acc, csi, pod, far, f1, bias)
-        except:
+        except Exception as e:
+            print('[NIMSLogger - print_stat] exception:', e)
             pass
 
         print(stat_str)
         print()
 
-        if test:
+        if mode == 'test':
             self._save_test_result()
 
         self._clear_one_target_stat(self.one_epoch_stat)
 
         return epoch_loss, epoch_stat
 
-    def latest_stat(self, target_time):
+    def latest_stat(self, target_time, mode):
         num_batch = target_time.shape[0]
-        batch_stn = self.num_stn * num_batch
+        if mode == 'train':
+            batch_points = self.num_pixels * num_batch
+        elif (mode == 'valid') or (mode == 'test'):
+            batch_points = self.num_stn * num_batch
+
         try:
-            accuracy = (self._latest_stat.correct / batch_stn) * 100
+            accuracy = (self._latest_stat.correct / batch_points) * 100
             assert accuracy <= 100.0
         except:
             pass
@@ -299,6 +310,7 @@ class NIMSLogger:
             pass
 
         self.num_update = 0
+        self.total_points = 0
 
     def _save_test_result(self):
         self.test_df = self.test_df.append(self.test_df.iloc[:, 2:].sum(axis=0), ignore_index=True)

nims_loss.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from pytorch_lightning.metrics.functional.classification import confusion_matrix
 
 import numpy as np
-from sklearn.metrics import confusion_matrix
 from sklearn.utils.class_weight import compute_class_weight
 
 __all__ = ['MSELoss', 'NIMSCrossEntropyLoss', 'NIMSBinaryFocalLoss']
@@ -21,21 +21,28 @@ class RMSELoss(nn.Module):
 class ClassificationStat(nn.Module):
     def __init__(self, num_classes=2):
         super().__init__()
-        self.classes = np.arange(num_classes)
+        self.num_classes = num_classes
 
-    def get_stat(self, preds, targets):
-        _, pred_labels = preds.topk(1, dim=1, largest=True, sorted=True)
-        b, _, num_stn = pred_labels.shape
-        assert (b, num_stn) == targets.shape
+    def get_stat(self, preds, targets, mode):
+        if mode == 'train':
+            _, pred_labels = preds.topk(1, dim=1, largest=True, sorted=True)
+            b = pred_labels.shape[0]
+
+            pred_labels = pred_labels.squeeze(1).detach().reshape(b, -1)
+            target_labels = targets.data.detach().reshape(b, -1)
+        elif (mode == 'valid') or (mode == 'test'):
+            _, pred_labels = preds.topk(1, dim=1, largest=True, sorted=True)
+            b, _, num_stn = pred_labels.shape
+            assert (b, num_stn) == targets.shape
+
+        pred_labels = pred_labels.squeeze(1).detach()
+        target_labels = targets.data.detach()
 
         correct = [0] * b
         hit = [0] * b
         miss = [0] * b
         fa = [0] * b
         cn = [0] * b
-
-        pred_labels = pred_labels.squeeze(1).detach().cpu().numpy()
-        target_labels = targets.data.detach().cpu().numpy()
 
         for i in range(b):
             pred, target = pred_labels[i], target_labels[i]
@@ -47,8 +54,9 @@ class ClassificationStat(nn.Module):
             - fp: False Alarm
             - tn: Correct Negative
             """
-            conf_mat = confusion_matrix(target, pred, labels=self.classes)
+            conf_mat = confusion_matrix(pred, target, num_classes=self.num_classes)
             _hit, _miss, _fa, _cn = conf_mat[1, 1], conf_mat[1, 0], conf_mat[0, 1], conf_mat[0, 0]
+            _hit, _miss, _fa, _cn = int(_hit), int(_miss), int(_fa), int(_cn)
             _correct = _hit + _cn
 
             correct[i] = _correct
@@ -101,7 +109,7 @@ class NIMSCrossEntropyLoss(ClassificationStat):
         return torch.from_numpy(weights).type(torch.FloatTensor).to(self.device)
 
     def forward(self, preds, targets, target_time,
-                stn_codi, logger=None, test=False):
+                stn_codi, mode, logger=None):
         """
         <Parameter>
         preds [torch.tensor]: NCHW format (N: batch size, C: class num)
@@ -119,18 +127,19 @@ class NIMSCrossEntropyLoss(ClassificationStat):
         # print('[cross_entropy] preds shape:', preds.shape)
         # print('[cross_entropy] targets shape:', targets.shape)
 
-        stn_preds = preds[:, :, stn_codi[:, 0], stn_codi[:, 1]]
-        stn_targets = targets[:, stn_codi[:, 0], stn_codi[:, 1]]
+        if (mode == 'valid') or (mode == 'test'):
+            preds = preds[:, :, stn_codi[:, 0], stn_codi[:, 1]]
+            targets = targets[:, stn_codi[:, 0], stn_codi[:, 1]]
+
+        correct, hit, miss, fa, cn = self.get_stat(preds, targets, mode=mode)
         
-        correct, hit, miss, fa, cn = self.get_stat(stn_preds, stn_targets)
-        
-        loss = F.cross_entropy(stn_preds, stn_targets, weight=class_weights, reduction='none')
+        loss = F.cross_entropy(preds, targets, weight=class_weights, reduction='none')
         loss = torch.mean(torch.mean(loss, dim=0))
 
         if logger:
             logger.update(loss=loss.item(), correct=correct,
                           hit=hit, miss=miss, fa=fa, cn=cn,
-                          target_time=target_time, test=test)
+                          target_time=target_time, mode=mode)
 
         # print('[cross_entropy] loss: {}'.format(loss.item()))
 

nims_trainer.py
@@ -94,13 +94,13 @@ class NIMSTrainer:
         for epoch in range(1, self.num_epochs + 1):
             # Run training epoch
             print('=' * 25, 'Epoch {} / {}'.format(epoch, self.num_epochs), '=' * 25)
-            self._epoch(self.train_loader, train=True, logger=self.nims_logger)
-            epoch_train_loss, epoch_train_stat = self.nims_logger.print_stat()
+            self._epoch(self.train_loader, mode='train', logger=self.nims_logger)
+            epoch_train_loss, epoch_train_stat = self.nims_logger.print_stat(mode='train')
 
             # Run validation epoch
             print('-' * 10, 'Validation', '-' * 10)
-            self._epoch(self.test_loader, train=False, valid=True, logger=self.nims_valid_logger)
-            epoch_valid_loss, epoch_valid_stat = self.nims_valid_logger.print_stat()
+            self._epoch(self.test_loader, mode='valid', logger=self.nims_valid_logger)
+            epoch_valid_loss, epoch_valid_stat = self.nims_valid_logger.print_stat(mode='valid')
 
             if epoch_valid_loss < self.trained_weight['best_loss']:
                 self.trained_weight['model']      = self.model.state_dict()
@@ -138,11 +138,11 @@ class NIMSTrainer:
 
         print('=' * 25, 'Test', '=' * 25)
         with torch.no_grad():
-            self._epoch(self.test_loader, train=False, logger=self.nims_logger)
+            self._epoch(self.test_loader, mode='test', logger=self.nims_logger)
 
-        self.nims_logger.print_stat(test=True)
+        self.nims_logger.print_stat(mode='test')
 
-    def _epoch(self, data_loader, train, valid=False, logger=None):
+    def _epoch(self, data_loader, mode, logger=None):
         pbar = tqdm(data_loader)
         for images, target, target_time in pbar:
             if self.model_name == 'unet' or \
@@ -170,12 +170,11 @@ class NIMSTrainer:
 
             output = self.model(images)
             loss = self.criterion(output, target, target_time,
-                                  stn_codi=self.stn_codi, logger=logger,
-                                  test=((not train) and (not valid)))
+                                  stn_codi=self.stn_codi, mode=mode, logger=logger)
             
-            if train:
+            if mode == 'train':
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
-            pbar.set_description(logger.latest_stat(target_time))
+            pbar.set_description(logger.latest_stat(target_time, mode=mode))

nims_util.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn as nn
+from torch.nn import DataParallel
 import torch.optim as optim
 from torch.utils.data import Subset
 import numpy as np
@@ -7,12 +7,9 @@ import torchvision.transforms as transforms
 
 from model.unet_model import UNet, AttentionUNet
 from model.conv_lstm import EncoderForecaster
-from model.persistence import Persistence
-
 from nims_loss import *
 
 import os
-import sys
 import random
 import argparse
 
@@ -26,26 +23,10 @@ try:
 except:
     pass
 
-__all__ = ['NIMSStat', 'create_results_dir', 'select_date', 'parse_args', 'set_device', 'undersample',
-           'fix_seed', 'set_model', 'set_optimizer', 'set_experiment_name', 'get_min_max_values', 'get_min_max_normalization']
+__all__ = ['NIMSStat', 'select_date', 'parse_args', 'set_device', 'undersample', 'fix_seed',
+           'set_model', 'set_optimizer', 'set_experiment_name', 'get_min_max_values', 'get_min_max_normalization']
 
 NIMSStat = namedtuple('NIMSStat', 'acc, csi, pod, far, f1, bias')
-
-def create_results_dir(experiment_name):
-    # Base results directory
-    results_dir = os.path.join('./results', experiment_name)
-    if not os.path.isdir(results_dir):
-        os.mkdir(results_dir)
-
-    # Create evaluation directory if not
-    eval_dir = os.path.join(results_dir, 'eval')
-    if not os.path.isdir(eval_dir):
-        os.mkdir(eval_dir)
-
-    # Create comparison_graph directory if not
-    graph_dir = os.path.join(results_dir, 'comparison_graph')
-    if not os.path.isdir(graph_dir):
-        os.mkdir(graph_dir)
 
 def select_date(test=False):
     # Mode selection
@@ -161,7 +142,7 @@ def parse_args():
     common.add_argument('--model', default='unet', type=str, help='which model to use [unet, attn_unet, convlstm]')
     common.add_argument('--dataset_dir', default='/home/osilab12/ssd/NIMS_LDPS', type=str, help='root directory of dataset')
     common.add_argument('--device', default='0', type=str, help='which device to use')
-    common.add_argument('--num_workers', default=6, type=int, help='# of workers for dataloader')
+    common.add_argument('--num_workers', default=5, type=int, help='# of workers for dataloader')
     common.add_argument('--eval_only', default=False, help='when enabled, do not run test epoch, only creating graph', action='store_true')
     common.add_argument('--custom_name', default=None, type=str, help='add customize experiment name')
     # common.add_argument('--debug', help='turn on debugging print', action='store_true')
@@ -354,7 +335,7 @@ def set_model(sample, device, args, train=True,
         checkpoint = torch.load(model_path)
         model.load_state_dict(checkpoint, strict=False)
 
-    model = nn.DataParallel(model)
+    model = DataParallel(model)
 
     return model, criterion
 

test.py
@@ -15,6 +15,41 @@ import time
 
 MONTH_DAY = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+#########################################################
+# 1. Function for test setting part                     #
+#########################################################
+
+def select_weight()
+    results_dir = os.path.join('./results')
+    experiment_list = sorted([f for f in os.listdir(results_dir) if f.startswith('nims-utc')])
+    print()
+    print('=' * 33, 'Which experiment do you want to test?', '=' * 33)
+    print()
+    print('-' * 100)
+    print('{:^4s}| {:^19s} | {:^65s}{:>7s}'.format('Idx', 'Last Modified', 'Trained Weight', '|'))
+    print('-' * 100)
+    for i, experiment in enumerate(experiment_list):
+        path_date = time.strftime('%Y/%m/%d %H:%M:%S', time.gmtime(os.path.getmtime(os.path.join(results_dir, experiment))))
+        print('[{:2d}] <{:s}> {}'.format(i + 1, path_date, experiment))
+    choice = int(input('\n> '))
+    experiment_name = experiment_list[choice - 1]
+    chosen_weight = torch.load(os.path.join(results_dir, experiment_list[choice - 1], 'trained_weight.pt'), map_location=device)
+    print('Load experiment... [{}] {}:'.format(choice, experiment_name))
+    print()
+
+    # Print trained weight info
+    print('=' * 25, 'Trained Weight Information', '=' * 25)
+    print('[best epoch]:', chosen_weight['best_epoch'])
+    print('[best loss ]: {:5f}'.format(chosen_weight['best_loss']))
+    print('[best acc  ]: {:5f}'.format(chosen_weight['best_acc']))
+    print('[best csi  ]: {:5f}'.format(chosen_weight['best_csi']))
+    print('[best pod  ]: {:5f}'.format(chosen_weight['best_pod']))
+    print('[best far  ]: {:5f}'.format(chosen_weight['best_far']))
+    print('[best f1   ]: {:5f}'.format(chosen_weight['best_f1']))
+    print('[best bias ]: {:5f}'.format(chosen_weight['best_bias']))
+
+    return chosen_weight, experiment_name
+
 def create_test_date_list(date, experiment_name):
     test_date_list = []
     test_months = list(range(date['start_month'], date['end_month'] + 1))
@@ -42,6 +77,10 @@ def create_test_date_list(date, experiment_name):
         test_date_list.append(current_test_path)
 
     return test_date_list
+
+#########################################################
+# 2. Function for plotting part                         #
+#########################################################
 
 def recreate_total_stat(total_test_path):
     one_day_stat_list = sorted([os.path.join(total_test_path, f) for f in os.listdir(total_test_path) if f.endswith('.csv')])
@@ -153,9 +192,6 @@ if __name__ == '__main__':
     # 1. Test setting part                                  #
     #########################################################
 
-    # Set the number of threads in pytorch
-    torch.set_num_threads(5)
-
     # Parsing command line arguments
     args = parse_args()
 
@@ -163,33 +199,7 @@ if __name__ == '__main__':
     device = set_device(args)
 
     # Specify trained model weight
-    results_dir = os.path.join('./results')
-    experiment_list = sorted([f for f in os.listdir(results_dir) if f.startswith('nims-utc0')])
-    print()
-    print('=' * 33, 'Which experiment do you want to test?', '=' * 33)
-    print()
-    print('-' * 100)
-    print('{:^4s}| {:^19s} | {:^65s}{:>7s}'.format('Idx', 'Last Modified', 'Trained Weight', '|'))
-    print('-' * 100)
-    for i, experiment in enumerate(experiment_list):
-        path_date = time.strftime('%Y/%m/%d %H:%M:%S', time.gmtime(os.path.getmtime(os.path.join(results_dir, experiment))))
-        print('[{:2d}] <{:s}> {}'.format(i + 1, path_date, experiment))
-    choice = int(input('\n> '))
-    experiment_name = experiment_list[choice - 1]
-    chosen_weight = torch.load(os.path.join(results_dir, experiment_list[choice - 1], 'trained_weight.pt'), map_location=device)
-    print('Load experiment... [{}] {}:'.format(choice, experiment_name))
-    print()
-
-    # Print trained weight info
-    print('=' * 25, 'Trained Weight Information', '=' * 25)
-    print('[best epoch]:', chosen_weight['best_epoch'])
-    print('[best loss ]: {:5f}'.format(chosen_weight['best_loss']))
-    print('[best acc  ]: {:5f}'.format(chosen_weight['best_acc']))
-    print('[best csi  ]: {:5f}'.format(chosen_weight['best_csi']))
-    print('[best pod  ]: {:5f}'.format(chosen_weight['best_pod']))
-    print('[best far  ]: {:5f}'.format(chosen_weight['best_far']))
-    print('[best f1   ]: {:5f}'.format(chosen_weight['best_f1']))
-    print('[best bias ]: {:5f}'.format(chosen_weight['best_bias']))
+    chosen_weight, experiment_name = select_weight()
 
     # Select start and end date for train
     date = select_date(test=True)
@@ -264,7 +274,7 @@ if __name__ == '__main__':
     # 2. Plotting part                                      #
     #########################################################
 
-    # Load evaluate results from test model
+    # 2-1. Plot stat graph for each month
     for current_test_path in test_date_list:
         _curr_date = current_test_path.split('/')[-1].split('-')[0]
         year, month = int(_curr_date[:4]), int(_curr_date[4:6])
@@ -282,7 +292,7 @@ if __name__ == '__main__':
         # Plot graph for each stat
         plot_stat_graph(ldaps_stat, model_stat, curr_date, experiment_name)
 
-    # Create directory for whole test date if start and end month is different
+    # 2-2. Plot stat graph for whole test range if start and end month is different
     if date['start_month'] != date['end_month']:
         total_test_date = '{:4d}{:02d}{:02d}-{:04d}{:02d}{:02d}'.format(date['year'], date['start_month'], date['start_day'],
                                                                         date['year'], date['end_month'], date['end_day'])